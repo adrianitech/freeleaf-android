3fb5de9c54f52294e8eb910551c19a47e2827bec
Adrian Mateoaea <adrianitech@outlook.com>
2013-09-16 17:58:29 +0300

    UI tweaks

--- FreeLeaf/src/com/adrian/freeleaf/MainActivity.java
+++ FreeLeaf/src/com/adrian/freeleaf/MainActivity.java
@@ -1,132 +1,37 @@
 package com.adrian.freeleaf;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.*;
-import android.content.res.Resources;
-import android.graphics.*;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v4.widget.SlidingPaneLayout;
 import android.view.*;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.*;
 import com.adrian.freeleaf.Utils.DiscoveryThread;
 
 public class MainActivity extends Activity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
-    private ImageView pulseImage;
-    private ImageButton buttonActivate;
-    private SlidingPaneLayout slidingPane;
-    private ListView listActions;
-
     private DiscoveryThread discoveryThread;
+
+    private TextView textOnOff;
+    private Switch switchDiscovery;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        // Get the controls
-        slidingPane = (SlidingPaneLayout)findViewById(R.id.slidingPane);
-        listActions = (ListView)findViewById(R.id.listActions);
-        pulseImage = (ImageView)findViewById(R.id.pulseImage);
-        buttonActivate = (ImageButton)findViewById(R.id.buttonActivate);
-
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         prefs.registerOnSharedPreferenceChangeListener(this);
-
-        // Set control options
-        slidingPane.setCoveredFadeColor(Color.TRANSPARENT);
-        slidingPane.setSliderFadeColor(Color.TRANSPARENT);
-        slidingPane.setParallaxDistance(100);
-
-
-        final Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        final int rad = size.x / 2;
-
-        Bitmap bmp = Bitmap.createBitmap(2*rad, 2*rad, Bitmap.Config.ARGB_8888);
-        Canvas c = new Canvas(bmp);
-
-        Paint p = new Paint();
-        p.setAntiAlias(true);
-        p.setDither(true);
-
-        p.setColor(Color.parseColor("#d3f696"));
-        p.setStyle(Paint.Style.FILL);
-        c.drawCircle(rad, rad, rad, p);
-
-        p.setColor(Color.parseColor("#b1df63"));
-        p.setStyle(Paint.Style.STROKE);
-        c.drawCircle(rad, rad, rad, p);
-
-        pulseImage.setImageBitmap(bmp);
-
-
-        buttonActivate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                discoveryThread.setIsActive(!discoveryThread.getIsActive());
-                CheckButtonState();
-            }
-        });
-
-
-        final Resources res = getResources();
-        String[] actions = res.getStringArray(R.array.menu_values);
-
-        listActions.setAdapter(new MenuAdapter1(this, android.R.layout.preference_category, actions));
-        listActions.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if(i == 1) {
-                    startActivity(new Intent(MainActivity.this, SettingsActivity.class));
-                } else if(i == 2) {
-
-                } else if(i == 3) {
-
-                } else if(i == 5) {
-
-                } else if(i == 6) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    builder.setTitle("Do you really want to exit?")
-                            .setMessage("If you exit now all the active file transfers will be intrerupted")
-                            .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
-                                    System.exit(0);
-                                }
-                            })
-                            .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
-                                }
-                            });
-                    AlertDialog alertDialog = builder.create();
-                    alertDialog.show();
-                }
-            }
-        });
 
         discoveryThread = new DiscoveryThread(this);
 
         final String refreshRate = prefs.getString("discovery_refresh", "3000");
         discoveryThread.setRefreshRate(Integer.parseInt(refreshRate));
-
         discoveryThread.setIsActive(prefs.getBoolean("auto_discovery", true));
-        CheckButtonState();
-
         discoveryThread.setUsername(prefs.getString("discovery_name", "Unknown"));
-
-        discoveryThread.setDataSentEventListener(new DiscoveryThread.OnDataSentEventListener() {
-            @Override
-            public void onDataSent() {
-                AnimatePulse();
-            }
-        });
-
         discoveryThread.start();
+
+        textOnOff = (TextView)findViewById(R.id.textOnOff);
 
 
 
@@ -143,33 +48,8 @@ public class MainActivity extends Activity implements SharedPreferences.OnShared
 
         wifiManager = (WifiManager)MainActivity.this.getSystemService(Context.WIFI_SERVICE);
         rssi = wifiManager.getConnectionInfo().getRssi();  */
-
-
-
-
-
     }
 
-    public void CheckButtonState() {
-        if(discoveryThread.getIsActive()) {
-            pulseImage.setVisibility(View.VISIBLE);
-            buttonActivate.setImageResource(R.drawable.ic_android_green);
-        } else {
-            pulseImage.setVisibility(View.INVISIBLE);
-            buttonActivate.setImageResource(R.drawable.ic_android_red);
-        }
-    }
-
-    private void AnimatePulse() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                pulseImage.clearAnimation();
-                final Animation pulseAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.pulse_animation);
-                pulseImage.setAnimation(pulseAnimation);
-            }
-        });
-    }
 
     /*BroadcastReceiver rec = new BroadcastReceiver() {
         @Override
@@ -191,76 +71,19 @@ public class MainActivity extends Activity implements SharedPreferences.OnShared
     }
 
     @Override
-    public boolean onKeyUp(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_MENU) {
-            if(slidingPane.isOpen()) {
-                slidingPane.closePane();
-            } else {
-                slidingPane.openPane();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        switchDiscovery = (Switch)menu.findItem(R.id.switchDiscovery).getActionView().findViewById(R.id.switchForActionBar);
+        switchDiscovery.setChecked(discoveryThread.getIsActive());
+        switchDiscovery.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                textOnOff.setText(b ? switchDiscovery.getTextOn() : switchDiscovery.getTextOff());
+                discoveryThread.setIsActive(b);
             }
-            return true;
-        }
-        return super.onKeyUp(keyCode, event);
-    }
+        });
 
-    private class MenuAdapter1 extends ArrayAdapter<String> {
-
-        private LayoutInflater mInflater;
-
-        private MenuAdapter1(Context context, int textViewResourceId, String[] objects) {
-            super(context, textViewResourceId, objects);
-            mInflater = LayoutInflater.from(context);
-        }
-
-        @Override
-        public boolean areAllItemsEnabled() {
-            return false;
-        }
-
-        @Override
-        public boolean isEnabled(int position) {
-            return !getItem(position).startsWith("*");
-        }
-
-        @Override
-        public int getItemViewType(int position) {
-            return getItem(position).startsWith("*") ? 0 : 1;
-        }
-
-        @Override
-        public int getViewTypeCount() {
-            return 2;
-        }
-
-        private class ViewHolder {
-            public TextView text;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View rowView = convertView;
-            ViewHolder holder;
-            String item = getItem(position);
-
-            if (rowView == null) {
-                holder = new ViewHolder();
-                if(item.startsWith("*")) {
-                    rowView = mInflater.inflate(R.layout.menu_group, null);
-                } else {
-                    rowView = mInflater.inflate(R.layout.menu_item, null);
-                }
-                holder.text = (TextView)rowView.findViewById(R.id.text1);
-                rowView.setTag(holder);
-            } else {
-                holder = (ViewHolder)rowView.getTag();
-            }
-
-            if(item.startsWith("*")) {
-                item = item.substring(1);
-            }
-            holder.text.setText(item);
-
-            return rowView;
-        }
+        return true;
     }
 }
